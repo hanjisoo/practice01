b4ed456723e53e28d9cbcabde6b6a8efddab5745
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-05 14:00:43 +0900

    2st

--- src/com/javaex/practice01/Prac01.java
+++ src/com/javaex/practice01/Prac01.java
@@ -1,5 +1,14 @@
 package com.javaex.practice01;
 
-public class Proj01 {
-
+public class Prac01 {
+   public static void main(String[] args) {
+	  
+	   for(int i=1;i<=100;i++) {
+		if(i%5==0&&i%7==0) {
+			System.out.println(i);
+		}
+		   	   
+	   }
+   }
+	
 }
---
+++ src/com/javaex/practice01/prac03.java
@@ -0,0 +1,17 @@
+package com.javaex.practice01;
+
+public class prac03 {
+
+	public static void main(String[] args) {
+	
+		for(int y=1; y<10; y++) {
+			for(int x=2; x<10; x++) {
+				System.out.print(x+"*"+y+"="+x*y+"\t");
+			
+			}
+			System.out.println();
+		}
+
+	}
+
+}
